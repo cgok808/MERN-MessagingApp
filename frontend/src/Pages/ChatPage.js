509c303e1f1737d52e6701336cb8b7c57d3b74a9
Chay <chaylin808@gmail.com>
2022-05-24 03:12:42 +0900

    make it better

---
+++ frontend/src/Pages/ChatPage.js
@@ -0,0 +1,27 @@
+import { Flex } from "@chakra-ui/react";
+import { useState } from "react";
+import ChatBox from "../components/ChatBox";
+import SideDrawer from "../components/miscellaneous/SideDrawer";
+import MyChats from "../components/MyChats";
+import { ChatState } from "../Context/ChatProvider";
+
+const ChatPage = () => {
+  const { user } = ChatState();
+  const [fetchAgain, setFetchAgain] = useState(false);
+
+  return (
+    <div style={{ width: "100%" }}>
+      {user && <SideDrawer />}
+      <Flex justifyContent='space-between' w='100%' h='91.5vh' p='10px'>
+        {user && (
+          <MyChats fetchAgain={fetchAgain} />
+        )}
+        {user && (
+          <ChatBox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
+        )}
+      </Flex>
+    </div>
+  );
+};
+
+export default ChatPage;
